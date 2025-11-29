c42de67ed71b77c78941fd67785f177930300d45
kercre123 <kerigancreighton@gmail.com>
2025-11-28 22:16:33 -0600

    pv shouldn't be required - let's just use wget's built-in progress bar

--- get-deps.sh
+++ get-deps.sh
@@ -2,25 +2,11 @@
 
 TOOLCHAIN_VER="5.3.0-r07"
 
-if [[ ! `which pv` ]]; then
-  echo "Install "pv" via your os's package manager"
-  echo "
-  # Arch Linux:
-  sudo pacman -S pv
-  # Ubuntu/Debian:
-  sudo apt-get update && sudo apt-get install -y pv
-  # Fedora
-  sudo dnf install -y pv"
-  exit 1
-fi
-
 if [[ ! -d ~/.anki/vicos-sdk/dist/$TOOLCHAIN_VER ]]; then
-  echo Getting toolchain version $TOOLCHAIN_VER...
-  mkdir ~/.anki/vicos-sdk/dist/$TOOLCHAIN_VER
+  echo "Getting toolchain version $TOOLCHAIN_VER..."
+  mkdir -p ~/.anki/vicos-sdk/dist/$TOOLCHAIN_VER
   cd ~/.anki/vicos-sdk/dist/$TOOLCHAIN_VER
-  wget https://github.com/os-vector/wire-os-externals/releases/download/$TOOLCHAIN_VER/vicos-sdk_"$TOOLCHAIN_VER"_amd64-linux.tar.gz
-  pv vicos-sdk_"$TOOLCHAIN_VER"_amd64-linux.tar.gz | tar -xz
-  rm vicos-sdk_"$TOOLCHAIN_VER"_amd64-linux.tar.gz
+  wget -q --show-progress https://github.com/os-vector/wire-os-externals/releases/download/$TOOLCHAIN_VER/vicos-sdk_"$TOOLCHAIN_VER"_amd64-linux.tar.gz -O - | tar -xz
   echo "Toolchain version $TOOLCHAIN_VER has been installed!"
   exit
 else
